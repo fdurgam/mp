659a2bb9c08b97fc31e8780c2423adaa18349395
fdurgam <fdurgam@gmail.com>
2024-07-08 19:52:59 -0300

    2025

--- index.js
+++ index.js
@@ -7,7 +7,33 @@ mercadopago.configure({
 	access_token: "TEST-4798754970937613-041720-cd7aa86b4def541abbee5ba18a204d92-133839272",
 });
 
+app.post("/create_preference", (req, res) => {
 
+	let preference = {
+		items: [
+			{
+				title: req.body.description,
+				unit_price: Number(req.body.price),
+				quantity: Number(req.body.quantity),
+			}
+		],
+		back_urls: {
+			"success": "http://localhost:8080/feedback",
+			"failure": "http://localhost:8080/feedback",
+			"pending": "http://localhost:8080/feedback"
+		},
+		auto_return: "approved",
+	};
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({
+				id: response.body.id
+			});
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
 const port=process.env.PORT || 3000
 app.get("/", (req, res) => {
 	const htmlResponse = `
@@ -16,7 +42,7 @@ app.get("/", (req, res) => {
 		  <title>NodeJs y Express en Vercel</title>
 		</head>
 		<body>
-		  <h1>Soy un proyecto Back end en vercel</h1>
+		  <h1>Soy un proyecto Back end en vercel22</h1>
 		</body>
 	  </html>
 	`;
